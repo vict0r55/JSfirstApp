d7ec0d76ed30e7eee4cd5dc20dc1cab70aa4d66e
Victor <victor.leyrus@yandex.ru>
2018-11-23 16:05:09 +0300

    create git

---
+++ js/script.js
@@ -0,0 +1,35 @@
+function myFirstApp(name, age) {
+
+	alert("Привет, меня зовут " + name + " и это моя двадцать первая программа!");
+
+	function showSkills() {
+		let skills = [
+		"html",
+		"css",
+		"js",
+		"php",
+		"java",
+		"xml"
+		];
+		console.log(skills);
+	}
+
+	function checkAge(){
+		if (age > 18) {
+			console.log('Вы можете смотреть "интересные" видео ;)');
+		} else {
+			console.log('Вы еще слишком малы для того что бы смотреть "интересные" видео!');
+		}
+	}
+
+	function calcPow(num) {
+		num *= 2;
+		console.log(num);
+	}
+
+	showSkills();
+	checkAge();
+	calcPow(4);
+}
+
+myFirstApp("Victor", 20);
